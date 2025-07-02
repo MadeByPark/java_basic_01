79374c5afd726ab797fe31fc140325a0ed8a6414
MadeByPark <dudwlsdl4817@gmail.com>
2025-07-02 14:11:20 +0900

    오리 2단계 완료

--- src/main/java/com/back.java
+++ src/main/java/com/back.java
@@ -2,16 +2,35 @@ package com;
 
 public class back {
     public static void main(String[] args) {
-        오리 a오리 = new 오리();
-        a오리.날다();
+        청둥오리 a청둥오리 = new 청둥오리();
+        a청둥오리.날다();
+
+        흰오리 a흰오리 = new 흰오리();
+        a흰오리.날다();
     }
 }
 
-    class 오리 {
-        void 날다() {
-            System.out.println("오리가 날아갑니다.");
-        }
+class 오리 {
+    void 날다() {
+        System.out.println("오리가 날아갑니다.");
     }
+}
+
+class 청둥오리  {
+    void 날다() {
+        System.out.println("청둥오리가 날아갑니다.");
+    }
+}
+
+class 흰오리  {
+    void 날다() {
+        System.out.println("흰오리가 날아갑니다.");
+    }
+}
+
+
+
+
 
 
 
