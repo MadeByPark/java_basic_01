1097c8c8366d45a68d5cbf115cacfde8c4e37d9f
MadeByPark <dudwlsdl4817@gmail.com>
2025-07-02 12:46:20 +0900

    오리 1단계 완료

---
+++ src/main/java/com/back.java
@@ -0,0 +1,17 @@
+package com;
+
+public class back {
+    public static void main(String[] args) {
+        오리 a오리 = new 오리();
+        a오리.날다();
+    }
+}
+
+    class 오리 {
+        void 날다() {
+            System.out.println("오리가 날아갑니다.");
+        }
+    }
+
+
+
